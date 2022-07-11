b36feee547ae3f485e06c28f2b12a1c0e8141dd0
DOLLAR1990 <DOLLAR1990@mail.ru>
2022-07-11 20:53:15 +0300

    сделали второе третье задание

---
+++ tsk3/Program.cs
@@ -0,0 +1,45 @@
+﻿Console.WriteLine("Введите день недели");
+int dayweek = Convert.ToInt32(Console.ReadLine());
+switch (dayweek)
+{
+    case 1:
+        {
+            Console.WriteLine($"День недели {dayweek} понедельник");
+            break;
+        }
+    case 2:
+        {
+            Console.WriteLine($"День недели {dayweek} Вторник");
+            break;
+        }
+    case 3:
+        {
+            Console.WriteLine($"День недели {dayweek} Среда");
+            break;
+        }
+    case 4:
+        {
+            Console.WriteLine($"День недели {dayweek} Четверг");
+            break;
+        }
+    case 5:
+        {
+            Console.WriteLine($"День недели {dayweek} Пятница ");
+            break;
+        }
+    case 6:
+        {
+            Console.WriteLine($"День недели {dayweek} Суббота");
+            break;
+        }
+    case 7:
+        {
+            Console.WriteLine($"День недели {dayweek} Воскресенье");
+            break;
+        }
+    default:
+        {
+            Console.WriteLine($"День недели {dayweek} не существует");
+            break;
+        }
+}
