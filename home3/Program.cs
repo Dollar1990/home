4e5a79516dac3974a10e3b5298aed38858d54ee8
DOLLAR1990 <DOLLAR1990@mail.ru>
2022-07-18 17:00:14 +0300

    домашка задача 10

---
+++ home3/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+//  456 -> 5
+//  782 -> 8
+//  918 -> 1 
+
+Console.Write("Введите трехзначное число число ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number < 100 || number > 999)
+{
+   Console.WriteLine($"Неверный ввод число {number} не трехзначное");
+}
+else
+{
+int number2 = (number % 100) / 10;
+Console.WriteLine($"{number} -> {number2}");
+}
