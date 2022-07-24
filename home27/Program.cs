4232ae7b7064207d76a1b444dae6f32021bca744
DOLLAR1990 <DOLLAR1990@mail.ru>
2022-07-24 16:21:09 +0300

    Домашнее задание задача 27

---
+++ home27/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+
+// 82 -> 10
+
+// 9012 -> 12
+
+int SumDigit(int digit)
+{
+    int sumDigit = 0;
+    while (digit > 0)
+    {
+        sumDigit = sumDigit + digit % 10;
+        digit = digit / 10;
+    }
+    return sumDigit;
+}
+
+Console.Write("Введите число в котором будем считать сумму всех цифр: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Сумма всех цифр в числе {number} -> {SumDigit(number)}");
